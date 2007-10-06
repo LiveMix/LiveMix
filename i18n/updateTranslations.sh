8f963bf21b69800484fdbc97dda1fbae86a0c817
Stéphane Brunner <courriel@stephane-brunner.ch>
2007-10-06 16:45:11 +0000

    Commiting the 0.45svn20071006 release of the 'livemix' project.
    git-svn-id: http://svn.codingteam.net/livemix/trunk@81 c9143dbb-82d1-40ab-9a6a-25cbcf1f4bdc

--- i18n/updateTranslations.sh
+++ i18n/updateTranslations.sh
@@ -9,14 +9,21 @@ CPP=`find . | grep "\.cpp$"`
 H=`find . | grep "\.h$"`
 FILES="$UI $CPP $H"
 
-CMD="$QTDIR/bin/lupdate-qt4 ${FILES} -ts"
+if test "x$QTDIR" = "x"; then
+  CMD="lupdate-qt4 ${FILES} -ts"
+else
+  CMD="$QTDIR/bin/lupdate-qt4 ${FILES} -ts"
+fi
 
 $CMD i18n/livemix_fr.ts
 
 echo "Creating *.qm files"
 cd i18n
-$QTDIR/bin/lrelease-qt4 *.ts
-
+if test "x$QTDIR" = "x"; then
+  lrelease-qt4 *.ts
+else
+  $QTDIR/bin/lrelease-qt4 *.ts
+fi
 
 echo "Stats"
 ./stats.py
